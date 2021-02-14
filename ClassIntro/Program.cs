5607cbeaa4648adf828078b7df540b2f7896836e
Mustafa <msaridayi.09@gmail.com>
2021-02-14 10:27:44 +0200

    16 ocak 2021

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Mustafa";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Mustafa Sarıdayı";
+            kurs2.IzlenmeOrani = 67;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Milda Sarıdayı";
+            kurs3.IzlenmeOrani = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "Python";
+            kurs4.Egitmen = "Buse Sarıdayı";
+            kurs4.IzlenmeOrani = 90;
+
+            //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+            }
+            
+
+        //Console.WriteLine("Hello World!");
+    }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmen { get; set; }
+
+        public int IzlenmeOrani { get; set; }
+
+
+
+    }
+}
